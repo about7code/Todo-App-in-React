49b07554896dc8b33877d50dc77c215a502a8a89
about7code <sanaullahhaifz@gmail.com>
2020-02-18 21:03:55 +0400

    Updates

---
+++ precache-manifest.8e3bbb9b837a459e2a4796724871609b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cdcaf12c1bf8379c9fe0663fbdac33",
+    "url": "/about7code/Todo-App-in-React.git/index.html"
+  },
+  {
+    "revision": "1229f7726eb5969c7f97",
+    "url": "/about7code/Todo-App-in-React.git/static/css/main.f0fdf01b.chunk.css"
+  },
+  {
+    "revision": "623c353dcb037609ef20",
+    "url": "/about7code/Todo-App-in-React.git/static/js/2.2d0f9acd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/about7code/Todo-App-in-React.git/static/js/2.2d0f9acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1229f7726eb5969c7f97",
+    "url": "/about7code/Todo-App-in-React.git/static/js/main.04680fc8.chunk.js"
+  },
+  {
+    "revision": "d3391dc075cea071fc4b",
+    "url": "/about7code/Todo-App-in-React.git/static/js/runtime-main.84bfe538.js"
+  }
+]);
