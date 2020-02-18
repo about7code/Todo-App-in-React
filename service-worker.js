7c072cb18a565fa32025d8459abecf2cc62eb39e
about7code <sanaullahhaifz@gmail.com>
2020-02-18 21:18:18 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about7code/Todo-App-in-React.git/precache-manifest.8e3bbb9b837a459e2a4796724871609b.js"
+  "//about7code.github.io/Todo-App-in-React/precache-manifest.e9e8a4c08389e890c65cf54b95740a23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/about7code/Todo-App-in-React.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//about7code.github.io/Todo-App-in-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
